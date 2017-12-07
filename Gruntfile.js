a02be90e6b7129c0d0d0ae8bf1f0ab3fab0746de
skotperez <voraginebramante@gmail.com>
2017-12-07 11:40:11 +0100

    Config Grunt and theme for Los Madriles site

--- Gruntfile.js
+++ Gruntfile.js
@@ -178,7 +178,7 @@ module.exports = function(grunt) {
                     src : ['style.css', 'assets/js/*.js', 'assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}']
                 },
                 options: {
-                    proxy: "http://huertos.localhost",
+                    proxy: "http://losmadriles.localhost",
                     watchTask: true,
                     browser: "firefox"
                 }
