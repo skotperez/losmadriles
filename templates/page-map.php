33e8b429125bf3b8fe87fdfdfaf2a5374ed7a1de
skotperez <voraginebramante@gmail.com>
2018-03-01 16:43:20 +0100

    Changes main content position in map template

--- templates/page-map.php
+++ templates/page-map.php
@@ -10,12 +10,6 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-	<?php while ( have_posts() ) : the_post(); ?>
-
-		<?php get_template_part( 'content', 'page' ); ?>
-
-	<?php endwhile; // end of the loop. ?>
-
 	<?php
 	// MAPS
 	////
@@ -43,6 +37,13 @@ get_header(); ?>
 			</div>
 		</section><!-- #gardens -->
 	<?php endif; ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'page' ); ?>
+
+	<?php endwhile; // end of the loop. ?>
+
 </main><!-- #main -->
 
 <?php get_footer(); ?>
